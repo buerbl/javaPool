dfd4be15906ad46e393be5b308f44912b4272475
boolean <837751454@qq.com>
2019-04-30 20:44:30 +0800

    trycatch

---
+++ src/main/java/YuTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @Author: boolean
+ * @Date: 2019/4/30 17:59
+ * @description:
+ */
+public class YuTest {
+    @Test
+    public void test(){
+        System.out.println(true||false);
+    }
+
+
+}
