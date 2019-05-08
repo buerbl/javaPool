050554cae1ba64a358ad368d89d9798f164b21f2
buer <837751454@qq.com>
2019-05-09 01:50:01 +0800

    大数值

--- src/main/java/数组.java
+++ src/main/java/数组.java
@@ -10,16 +10,8 @@ import java.util.Arrays;
 public class 数组 {
     @Test
     public  void test(){
-        int[] a = new int[100];
-        System.out.println(Arrays.toString(a));
-        int[] b = new int[10];
-        System.out.println(Arrays.toString(b));
-        int[] c = new int[10];
-        System.out.println(c);
-        //匿名创建数组
-        new  int[] {1,2};
-        gffg
-        p
+
+
 
     }
 }
