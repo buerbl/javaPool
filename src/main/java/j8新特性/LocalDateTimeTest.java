1c901f003171acc77625ebe9919f93cfce167fde
buer <837751454@qq.com>
2019-06-08 23:02:15 +0800

    LocalDateTime使用

--- src/main/java/j8新特性/LocalDateTimeTest.java
+++ src/main/java/j8新特性/LocalDateTimeTest.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
+import java.util.Date;
 
 /**
  * @author buer
@@ -14,10 +17,17 @@ import java.time.LocalDateTime;
 public class LocalDateTimeTest {
     private final  static Logger logger = LoggerFactory.getLogger(LocalDateTimeTest.class);
 
+    //获取当前时间
     @Test
     public void test(){
         LocalDateTime now = LocalDateTime.now();
         logger.debug("sssss");
         logger.info("当前时间：【{}】", now);
+    }
+
+    //date转localdatetime 信你个龟
+    @Test
+    public void transformTest(){
+//        logger.debug("转了之后的时间：", LocalDateTime.from());
     }
 }
