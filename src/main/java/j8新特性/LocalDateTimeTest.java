9be66b720aca198dd69cd45fabcdf82bfd270b79
buer <837751454@qq.com>
2019-06-08 18:11:31 +0800

    for增强型循环

---
+++ src/main/java/j8新特性/LocalDateTimeTest.java
@@ -0,0 +1,22 @@
+package j8新特性;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author buer
+ * create 2019/6/8 18:06
+ * description 新时间联系
+ */
+public class LocalDateTimeTest {
+    private final  static Logger logger = LoggerFactory.getLogger(LocalDateTimeTest.class);
+
+    @Test
+    public void test(){
+        LocalDateTime now = LocalDateTime.now();
+        logger.debug("当前时间：【{}】", now);
+    }
+}
