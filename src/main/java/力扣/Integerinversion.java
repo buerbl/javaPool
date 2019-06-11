ae862adc5f28303587a5ae788b9612b880dd5745
boolean <837751454@qq.com>
2019-06-11 21:31:48 +0800

    发现规律

--- src/main/java/力扣/Integerinversion.java
+++ src/main/java/力扣/Integerinversion.java
@@ -8,6 +8,8 @@ import org.junit.Test;
  * description:
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
  * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2 (31),  2 (31) − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ *
+ * 规律：a*10+b
  */
 public class Integerinversion {
     public int reverse(int x) {
@@ -37,6 +39,7 @@ public class Integerinversion {
     }
     @Test
     public void test(){
-        reverse(12);
+
+        System.out.println(Integer.MIN_VALUE-1);
     }
 }
