56726fa79c9ae39d47c146dfbd597af71428b88f
buer <837751454@qq.com>
2019-08-26 00:59:39 +0800

    io练习

---
+++ src/main/java/输入与输出/IO.java
@@ -0,0 +1,20 @@
+package 输入与输出;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class IO {
+    @Test
+    public void test(){
+        //当前
+        File file = new File(".");
+        String[] list = file.list();
+        System.out.println(Arrays.toString(list));
+        //桌面
+        File file1 = new File("/");
+        System.out.println(Arrays.toString(file1.list()));
+    }
+}
