52b358517aa2edaeb79776ee2d38045195f31ca9
buerbl <18319748706@163.com>
2020-04-30 16:57:28 +0800

    判断空值

--- src/main/java/基础/ForTest.java
+++ src/main/java/基础/ForTest.java
@@ -1,14 +1,18 @@
 package 基础;
 
+import org.junit.Test;
+
 /**
  * @Description: for循环测试
- *
+ * <p>
  * 初始值不符合判断的话，不会执行循环 一次也不会
  * @Author: boolean
  * @Date: 2019/11/22 15:45
  */
 public class ForTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void main() {
         for (int i = 0; i < 10; i++) {
             System.out.println(-1);
             System.out.println("我是循环");
@@ -16,6 +20,15 @@ public class ForTest {
         }
         System.out.println("return后的位置");
     }
+
+    @Test
+    public void test1(){
+        for (int i = 0;;){
+            System.out.println(i);
+        }
+    }
+
+
 }
 
 
