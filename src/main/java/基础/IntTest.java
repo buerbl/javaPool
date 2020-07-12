0c83100801f6a1187321384b6b497c30f5e4ccf5
buerbl <18319748706@163.com>
2020-07-12 09:35:14 +0800

    :star:list test

--- src/main/java/基础/IntTest.java
+++ src/main/java/基础/IntTest.java
@@ -5,13 +5,29 @@ import lombok.Data;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author boolean
  * Date: 2019/5/29 9:54
  * description: 测试idea的反编译
  */
+@Data
 public   class IntTest {
+    // 测试初始化
+    private Integer code;
+    private String name;
+    private int num;
+    private List<String > list;
+
+    @Test
+    public void testInit(){
+        System.out.println(code);
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(list);
+    }
+
     @Test
     public  void test() throws Exception{
         String name = "a";
--- src/main/java/容器/ListTest.java
+++ src/main/java/容器/ListTest.java
@@ -2,7 +2,9 @@ package 容器;
 
 import org.junit.Test;
 
+import java.beans.Transient;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author boolean
@@ -33,4 +35,75 @@ public class ListTest {
     public void linkListTest(){
         LinkedList<String> linkedList = new LinkedList<>();
     }
+
+    @Test
+    public void testFor(){
+        int times = 10;
+        List<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        System.out.println(list);
+        for (int i = 0; i < list.size() - 1; i++){
+            for (int j = list.size() - 1;j > i; j --  ){
+                if (list.get(i) == list.get(j)){
+                    list.remove(i);
+                }
+            }
+        }
+        System.out.println(list);
+    }
+
+
+    @Test
+    public void testFor3(){
+        int times = 10;
+        List<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        System.out.println(list);
+        for (int i = 0; i < list.size() ; i++){
+            for (int j = i+1;j < list.size(); j ++  ){
+                if (list.get(i) == list.get(j)){
+                    list.remove(i);
+
+                }
+            }
+        }
+        System.out.println(list);
+    }
+
+
+    @Test
+    public void testFor1(){
+        int times = 10;
+        List<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        System.out.println(list);
+        for (int i = list.size() - 1; i >= 0; i--){
+            list.remove(i);
+        }
+        System.out.println(list);
+    }
+
+    @Test
+    public void testFor2(){
+        int times = 10;
+        List<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        System.out.println(list);
+        List<Integer> collect = list.stream().distinct().collect(Collectors.toList());
+        System.out.println(collect);
+    }
+
+
 }
