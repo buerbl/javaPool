e9adf8fe1be3371b23ddbd441ed2cc4f1285e660
buerbl <18319748706@163.com>
2020-08-13 09:35:02 +0800

    add

--- src/main/java/基础/DateTest.java
+++ src/main/java/基础/DateTest.java
@@ -1,8 +1,11 @@
 package 基础;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.util.Calendar;
@@ -13,6 +16,7 @@ import java.util.Date;
  * @create 2019/4/28 19:39
  * @description 时间描述
  */
+@Slf4j
 public class DateTest {
     private static final int a = 1;
     @Test
@@ -45,5 +49,30 @@ public class DateTest {
         Date zero = calendar.getTime();
         System.out.println(zero);
 
+    }
+
+
+
+    @Test
+    public void testDateException(){
+        System.out.println(inTimeByHMS("21:2100000io:11", "11:11:11", "23:11:11"));
+    }
+
+    public static Boolean inTimeByHMS (String targetTime, String startTime, String endTime){
+        Boolean result = Boolean.FALSE;
+        DateFormat df = new SimpleDateFormat("HH:mm:ss");
+        try {
+            Date dt1 = df.parse(targetTime);
+            Date dt2 = df.parse(startTime);
+            Date dt3 = df.parse(endTime);
+            if (dt1.getTime() >= dt2.getTime() && dt1.getTime() <= dt3.getTime()){
+                result = Boolean.TRUE;
+            }else {
+                result = Boolean.FALSE;
+            }
+        } catch (ParseException e) {
+            log.error("时间错误", e);
+        }
+        return result;
     }
 }
