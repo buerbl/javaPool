82a49bd0e1336adcdfd4a6de3f4c3a1eddadd5e7
buerbl <18319748706@163.com>
2020-10-21 17:09:28 +0800

    :star:add

--- src/main/java/容器/LinkListTest.java
+++ src/main/java/容器/LinkListTest.java
@@ -1,7 +1,11 @@
 package 容器;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -10,6 +14,7 @@ import java.util.LinkedList;
  * @Author: boolean
  * @Date: 2019/11/20 12:03
  */
+@Slf4j
 public class LinkListTest {
     LinkedList linkedList = new LinkedList();
     @Test
@@ -37,6 +42,12 @@ public class LinkListTest {
         j = i++;
         System.out.println(j);
         System.out.println(i);
+    }
+
+    @Test
+    public void test3(){
+        ArrayList<String> strings = Lists.newArrayList("2", "d", "d");
+        log.info(JSON.toJSONString(strings));
     }
 }
 
